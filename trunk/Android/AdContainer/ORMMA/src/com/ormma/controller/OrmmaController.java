07aba02c23129ec2b05b0d5045c71fdcf278ef07
jsodos@weather.com <jsodos@weather.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2010-09-21 12:28:05 +0000

    Removing JSONHelper. No longer used.

--- trunk/Android/AdContainer/ORMMA/src/com/ormma/controller/OrmmaController.java
+++ trunk/Android/AdContainer/ORMMA/src/com/ormma/controller/OrmmaController.java
@@ -27,7 +27,6 @@ import android.view.Surface;
 import android.view.View;
 import android.view.WindowManager;
 
-import com.ormma.util.JSONHelper;
 import com.ormma.view.OrmmaView;
 
 public class OrmmaController {
