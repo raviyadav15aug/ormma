cf84a4f2d91aa7fa465cd0cefcde58a9b8b99da9
robert.hedin@gmail.com <robert.hedin@gmail.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2010-10-18 13:13:10 +0000

    added initial data access layer

---
+++ trunk/iOS/AdContainer/ORMMA/Private/Data Model/ORMMADataAccessLayer.h
@@ -0,0 +1,26 @@
+//
+//  ORMMADataAccessLayer.h
+//  ORMMA
+//
+//  Created by Robert Hedin on 10/15/10.
+//  Copyright 2010 The Weather Channel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FMDatabase.h"
+
+
+@interface ORMMADataAccessLayer : NSObject 
+{
+@private
+	FMDatabase *m_database;
+}
+
+// designated accessor for the singleton instance
++ (ORMMADataAccessLayer *)sharedInstance;
+
+
+- (BOOL)open;
+- (void)close;
+
+@end
