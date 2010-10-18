4c131eb2b410a444ccbbdc7cb5650665d8762240
robert.hedin <robert.hedin@gmail.com>
2010-10-18 13:11:20 +0000

    added new cache implementation served via HTTP

---
+++ iOS/AdContainer/ORMMA/Private/Cache Implementations/ORMMALocalServer.h
@@ -0,0 +1,58 @@
+//
+//  ORMMALocalServer.h
+//  ORMMA
+//
+//  Created by Robert Hedin on 10/15/10.
+//  Copyright 2010 The Weather Channel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HTTPServer.h"
+#import "ASIHTTPRequest.h"
+#import "ORMMADataAccessLayer.h"
+
+
+
+@protocol ORMMALocalServerDelegate
+
+@required
+
+- (void)cacheFailed:(NSURL *)baseURL
+		  withError:(NSError *)error;
+
+- (void)cachedBaseURL:(NSURL *)baseURL
+				onURL:(NSURL *)url
+			   withId:(NSUInteger)creativeId;
+
+@end
+
+
+
+@interface ORMMALocalServer : NSObject <ASIHTTPRequestDelegate>
+{
+@private
+	HTTPServer *m_server;
+	ORMMADataAccessLayer *m_dal;
+}
+@property( nonatomic, copy, readonly ) NSString *cacheRoot;
+
+
+// designated accessor for the singleton instance
++ (ORMMALocalServer *)sharedInstance;
+
+
+// used to cache a specific URL
+- (void)cacheURL:(NSURL *)url
+	withDelegate:(id<ORMMALocalServerDelegate>)delegate;
+
+
+// used to cache local HTML
+- (void)cacheHTML:(NSString *)html
+		  baseURL:(NSURL *)baseURL
+	 withDelegate:(id<ORMMALocalServerDelegate>)delegate;
+
+
+- (NSString *)cachePathFromURL:(NSURL *)url;
+
+
+@end
---
+++ iOS/AdContainer/ORMMA/Private/Cache Implementations/ORMMALocalServer.m
@@ -0,0 +1,246 @@
+//
+//  ORMMALocalServer.m
+//  ORMMA
+//
+//  Created by Robert Hedin on 10/15/10.
+//  Copyright 2010 The Weather Channel. All rights reserved.
+//
+
+#import "ORMMALocalServer.h"
+
+
+
+@interface ORMMALocalServer ()
+
+- (NSString *)resourcePathFromBaseURL:(NSURL *)url;
+
+@end
+
+
+
+
+@implementation ORMMALocalServer
+
+
+#pragma mark -
+#pragma mark Constants
+
+NSString * const kORMMALocalServerWebRoot = @"ormma-web-root";
+NSString * const kORMMALocalServerDelegateKey = @"delegate";
+
+
+
+#pragma mark -
+#pragma mark Properties
+
+@dynamic cacheRoot;
+
+
+
+#pragma mark -
+#pragma mark Initializers / Memory Management
+
+
++ (ORMMALocalServer *)sharedInstance
+{
+	static ORMMALocalServer *sharedInstance = nil;
+
+    @synchronized( self )
+    {
+        if ( sharedInstance == nil )
+		{
+			sharedInstance = [[ORMMALocalServer alloc] init];
+		}
+    }
+    return sharedInstance;
+}
+
+
+- (ORMMALocalServer *)init
+{
+	if ( ( self = [super init] ) )
+	{
+		// Setup Access to the database
+		m_dal = [ORMMADataAccessLayer sharedInstance];
+		
+		// setup our Internal HTTP Server
+		NSError *error = nil;
+		m_server = [[HTTPServer alloc] init];
+		NSURL *url = [NSURL fileURLWithPath:[self cacheRoot]];
+		[m_server setDocumentRoot:url];
+		[m_server start:&error];
+		
+		// make sure the root path exists
+		NSFileManager *fm = [NSFileManager defaultManager];
+		[fm createDirectoryAtPath:self.cacheRoot 
+	  withIntermediateDirectories:YES 
+					   attributes:nil 
+							error:NULL];
+		
+	}
+	return self;
+}
+
+
+- (void)dealloc
+{
+	// shutdown our server
+	[m_server stop];
+	[m_server release], m_server = nil;
+	[super dealloc];
+}
+
+
+
+#pragma mark -
+#pragma mark Properties
+
+- (NSString *)cacheRoot
+{
+	// determine the root where our cache will be stored
+    NSArray *systemPaths = NSSearchPathForDirectoriesInDomains( NSCachesDirectory, NSUserDomainMask, YES ); 
+    NSString *basePath = [systemPaths objectAtIndex:0]; 
+	
+	// add the root
+	NSString *path = [basePath stringByAppendingPathComponent:kORMMALocalServerWebRoot];
+	
+	return path;
+}
+
+
+
+#pragma mark -
+#pragma mark Cache Loading
+
+- (void)cacheURL:(NSURL *)url
+	withDelegate:(id<ORMMALocalServerDelegate>)delegate;
+{
+	// setup our dictionary for the callback
+	NSDictionary *userInfo = [NSDictionary dictionaryWithObjectsAndKeys:delegate, kORMMALocalServerDelegateKey,
+							  nil];
+	
+	// this should retrieve the data from the specified URL
+	ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+	request.delegate = self;
+	request.userInfo = userInfo;
+	[request startAsynchronous];
+}
+
+
+- (void)cacheHTML:(NSString *)html
+		  baseURL:(NSURL *)baseURL
+	 withDelegate:(id<ORMMALocalServerDelegate>)delegate;
+{
+	// determine the hash for this creative
+	NSUInteger creativeId = [html hash];
+	NSString *path = [self.cacheRoot stringByAppendingPathComponent:[NSString stringWithFormat:@"%u", creativeId]];
+	NSString *fqpn = [path stringByAppendingPathComponent:@"index.html"];
+	
+	// see if we already have this creative cached
+	NSFileManager *fm = [NSFileManager defaultManager];
+	if ( ![fm fileExistsAtPath:fqpn] )
+	{
+		// we don't have it yet
+		// make sure the directory exists
+		[fm createDirectoryAtPath:path 
+	  withIntermediateDirectories:YES 
+					   attributes:nil 
+							error:NULL];
+		
+		// write the file to disk
+		NSData *data = [html dataUsingEncoding:NSUTF8StringEncoding];
+		[data writeToFile:fqpn
+			   atomically:YES];
+	}
+	
+	// Now, notify the delegate that we've saved the resource
+	NSString *urlString = [NSString stringWithFormat:@"http://localhost:%i/%u/index.html", [m_server port], 
+																						   creativeId];
+	NSURL *url = [NSURL URLWithString:urlString];
+	[delegate cachedBaseURL:baseURL
+					 onURL:url
+					 withId:creativeId];
+}
+
+
+#pragma mark -
+#pragma mark ASI HTTP Request Delegate
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+	// get the HTML
+	NSString *html = [request responseString];
+	
+	// determine the base URL to use for storage
+	NSURL *baseURL = [request originalURL];
+	
+	//store the retrieved data
+	id<ORMMALocalServerDelegate> d = (id<ORMMALocalServerDelegate>)[request.userInfo objectForKey:kORMMALocalServerDelegateKey];
+	[self cacheHTML:html
+			baseURL:baseURL
+	   withDelegate:d];
+}
+
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+	// get the error
+	NSError *error = request.error;
+	
+	// notify the delegate that the request failed
+	id<ORMMALocalServerDelegate> delegate = (id<ORMMALocalServerDelegate>)[request.userInfo objectForKey:kORMMALocalServerDelegateKey];
+	[delegate cacheFailed:[request originalURL]
+				withError:error];
+}
+
+
+
+#pragma mark -
+#pragma mark Utility
+
+// our resource path is: host + path1 + ... + pathN + resource
+- (NSString *)resourcePathFromBaseURL:(NSURL *)url
+{
+	// start with the host
+	NSMutableString *path = [NSMutableString stringWithCapacity:200];
+	[path appendString:[url host]];
+	
+	// add all but the actual resource
+	NSArray *pathComponents = [url pathComponents];
+	for ( NSInteger index = 0; index < ( pathComponents.count - 2 ); index++ )
+	{
+		NSString *component = [pathComponents objectAtIndex:index];
+		[path appendFormat:@"/%@", component];
+	}
+	
+	// now make sure the path exists
+	NSFileManager *fm = [NSFileManager defaultManager];
+	[fm createDirectoryAtPath:path 
+  withIntermediateDirectories:YES 
+				   attributes:nil 
+						error:NULL];
+
+	// now finish off the path
+	[path appendFormat:@"/%@", [url lastPathComponent]];
+	return path;
+}
+
+
+// our cache path will end up being
+// ROOT + host + path1 + ... + pathN + resource
+- (NSString *)cachePathFromURL:(NSURL *)url
+{
+	// add the root
+	NSString *path = self.cacheRoot;
+	
+	// add the resource path
+	NSString *rp = [self resourcePathFromBaseURL:url];
+	
+	path = [path stringByAppendingPathComponent:rp];
+	
+	// done
+	return path;
+}
+
+
+@end
