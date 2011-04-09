994e0cc4abb407015a9f351bfd9b21f74b7ed79d
philippelaporte1 <philippelaporte1@gmail.com>
2011-04-09 00:32:11 +0000

    added proper file headers

--- iOS/AdContainer/ORMMA/TestBed/main.m
+++ iOS/AdContainer/ORMMA/TestBed/main.m
@@ -1,10 +1,10 @@
-//
-//  main.m
-//  ORMMATestBed
-//
-//  Created by Robert Hedin on 9/8/10.
-//  Copyright The Weather Channel 2010. All rights reserved.
-//
+/*  Copyright (c) 2011 The ORMMA.org project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 
 #import <UIKit/UIKit.h>
 
