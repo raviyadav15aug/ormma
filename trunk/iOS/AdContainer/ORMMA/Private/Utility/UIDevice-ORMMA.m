ecbe70b1443a53e7f0b83df934d0529c5c2ea771
philippelaporte1@gmail.com <philippelaporte1@gmail.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2011-04-09 00:32:11 +0000

    added proper file headers

--- trunk/iOS/AdContainer/ORMMA/Private/Utility/UIDevice-ORMMA.m
+++ trunk/iOS/AdContainer/ORMMA/Private/Utility/UIDevice-ORMMA.m
@@ -1,10 +1,10 @@
-//
-//  UIDevice-ORMMA.m
-//  ORMMA
-//
-//  Created by Robert Hedin on 11/15/10.
-//  Copyright 2010 The Weather Channel. All rights reserved.
-//
+/*  Copyright (c) 2011 The ORMMA.org project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 
 #import "UIDevice-ORMMA.h"
 
--- trunk/iOS/AdContainer/ORMMA/Private/Utility/UIWebView-ORMMA.h
+++ trunk/iOS/AdContainer/ORMMA/Private/Utility/UIWebView-ORMMA.h
@@ -1,9 +1,10 @@
-//
-//  UIWebView-TWC.h
-//
-//  Created by Patrick Childers on 8/26/10.
-//  Copyright 2010 The Weather Channel. All rights reserved.
-//
+/*  Copyright (c) 2011 The ORMMA.org project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 
 #import <UIKit/UIKit.h>
 
