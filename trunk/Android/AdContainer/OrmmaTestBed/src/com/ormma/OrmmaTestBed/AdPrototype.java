8820244db4d032d92d9cf521798484f01d02afe2
philippelaporte1@gmail.com <philippelaporte1@gmail.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2011-05-04 21:40:35 +0000

    import cleanup

--- trunk/Android/AdContainer/OrmmaTestBed/src/com/ormma/OrmmaTestBed/AdPrototype.java
+++ trunk/Android/AdContainer/OrmmaTestBed/src/com/ormma/OrmmaTestBed/AdPrototype.java
@@ -1,17 +1,12 @@
 package com.ormma.OrmmaTestBed;
-
-import org.ormma.controller.OrmmaAssetController;
 import org.ormma.view.OrmmaView;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.TextView;
+
 
 public class AdPrototype extends Activity {
     private OrmmaView mAdView; 
