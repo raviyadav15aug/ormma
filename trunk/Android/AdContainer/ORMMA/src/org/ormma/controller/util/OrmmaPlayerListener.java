bc25d4e9a796e73f4431b13ccb250b34a3552164
philippelaporte1@gmail.com <philippelaporte1@gmail.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2011-05-04 20:59:54 +0000

    package name changes event creation update expand updates

---
+++ trunk/Android/AdContainer/ORMMA/src/org/ormma/controller/util/OrmmaPlayerListener.java
@@ -0,0 +1,25 @@
+package org.ormma.controller.util;
+
+/**
+ * 
+ * Interface class to receive call backs from Player
+ *
+ */
+public interface OrmmaPlayerListener {	
+	
+	/**
+	 * On completion
+	 */
+	public void onComplete();
+	
+	/**
+	 * On loading complete
+	 */
+	public void onPrepared();
+	
+	/**
+	 * On Error
+	 */
+	public void onError();
+}
+
