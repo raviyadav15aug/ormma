9210e57cebd39238564a73150cab72e45974061a
philippelaporte1@gmail.com <philippelaporte1@gmail.com@030e123b-9455-ebd1-faa8-74879600f9ee>
2011-05-05 22:02:31 +0000

    first step towards embedded mapviews

--- trunk/Android/AdContainer/OrmmaTestBed/src/com/ormma/OrmmaTestBed/AdPrototype.java
+++ trunk/Android/AdContainer/OrmmaTestBed/src/com/ormma/OrmmaTestBed/AdPrototype.java
@@ -1,50 +1,59 @@
+/*  Copyright (c) 2011 The ORMMA.org project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree. All contributing project authors may
+ *  be found in the AUTHORS file in the root of the source tree.
+ */
 package com.ormma.OrmmaTestBed;
+
+
 import org.ormma.view.OrmmaView;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+import com.google.android.maps.MapActivity;
 
-public class AdPrototype extends Activity {
+
+
+public class AdPrototype extends MapActivity {
     private OrmmaView mAdView; 
     private Button mButton;
-	/** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         mAdView = (OrmmaView) findViewById(R.id.ad);
-        
-     //   mAdView.setAssetController(new OrmmaAssetController(mAdView, mAdView.getContext()));
-//        mAdView.loadUrl("http://10.0.2.2/3.html"); 
+     
    	    mButton = (Button) findViewById(R.id.load);
 	    mButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
 //				Uri uri = Uri.parse("http://v.imwx.com/v/mobile-3gp/special/15_gwc_sears_friday_pr6136.3gp");
-//				Intent intent = new Intent();  
-//		        intent.setAction(android.content.Intent.ACTION_VIEW);  videoOptions_480x270.html
-//		        intent.setDataAndType(uri, "*/*");    http://demo.goldspotmedia.com/agencytest/test1.html
-//		        startActivity(intent);   file:///android_asset/320x50Expand_480wTransparency.html
+//			    videoOptions_480x270.html
+//		        http://demo.goldspotmedia.com/agencytest/test1.html
+//		        file:///android_asset/320x50Expand_480wTransparency.html
 				mAdView.loadUrl("http://demo.goldspotmedia.com/agencytest/test1.html");		
-				//mAdView.loadUrl("file:///android_asset/320x50Expand_480wTransparency.html");	
 			}
 		});
     }
     
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-//    	((OrmmaView)findViewById(R.id.ad)).saveState(outState); 
     	super.onSaveInstanceState(outState);
     }
     
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-//    	((OrmmaView)findViewById(R.id.ad)).restoreState(savedInstanceState); 
     	super.onRestoreInstanceState(savedInstanceState);
     }
+
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
 }
